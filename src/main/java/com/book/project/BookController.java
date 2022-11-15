c92ff930d87c96f1e81c3cab5259d7eab83bce78
Shafiul23 <shafiulmirza@hotmail.com>
2022-11-15 08:28:22 +0000

    get all books edited

--- src/main/java/com/book/project/BookController.java
+++ src/main/java/com/book/project/BookController.java
@@ -1,21 +1,52 @@
 package com.book.project;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 @AllArgsConstructor
 public class BookController {
 
+    @Autowired
+    BookRepository bookRepository;
+
     private final BookService bookService;
 
-    @GetMapping
-    public List<Book> fetchAllBooks(){
-        return bookService.getAllBooks();
+    @GetMapping("/books")
+//    public List<Book> fetchAllBooks(){
+//        return bookService.getAllBooks();
+    public ResponseEntity<List<Book>> getAllBooks(@RequestParam(required = false) String title){
+        try{
+            List<Book> books = new ArrayList<Book>();
+
+            if(title == null)
+                books.addAll(bookRepository.findAll());
+
+            if(books.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
